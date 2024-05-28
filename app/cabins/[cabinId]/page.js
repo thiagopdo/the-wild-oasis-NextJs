82f3a2e8643654d0ca9134250ce75c67d44ae815
thiagopdo <thiago.falconer@gmail.com>
2024-05-28 18:58:14 -0300

    feat: added dynamic metadata for cabin

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,6 +2,11 @@ import { getCabin } from "@/app/_lib/data-service";
 import Image from "next/image";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinId);
+  return { title: `Cabin ${name}` };
+}
+
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
 
