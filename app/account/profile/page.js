862abeba674c75a636b602b576e6c0b756e7d817
thiagopdo <thiago.falconer@gmail.com>
2024-06-06 16:48:01 -0300

    fix: changed server component in client component

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,27 +1,33 @@
 import SelectCountry from "@/app/_components/SelectCountry";
-
+import UpdateProfile from "@/app/_components/UpdateProfile";
 
 export const metadata = {
-  title: "Update Profile"
-}
+  title: "Update Profile",
+};
 
 export default function Page() {
   // CHANGE
+
   const countryFlag = "pt.jpg";
   const nationality = "portugal";
-
   return (
     <div>
-      <h2 className="font-semibold text-2xl text-accent-400 mb-4">
+      <h2 className="mb-4 text-2xl font-semibold text-accent-400">
         Update your guest profile
       </h2>
 
-      <p className="text-lg mb-8 text-primary-200">
+      <p className="mb-8 text-lg text-primary-200">
         Providing the following information will make your check-in process
         faster and smoother. See you soon!
       </p>
-
-      
+      <UpdateProfile>
+        <SelectCountry
+          name="nationality"
+          id="nationality"
+          className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm"
+          defaultCountry={nationality}
+        />
+      </UpdateProfile>
     </div>
   );
 }
