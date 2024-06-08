4e196e82aa8dc8d992643c1c55043cde3fc9fab6
thiagopdo <thiago.falconer@gmail.com>
2024-06-08 02:17:53 -0300

    feat: added user session to the Navigation comp

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,26 +1,51 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+  console.log(session);
+
   return (
     <nav className="z-10 text-xl">
-      <ul className="flex gap-16 items-center">
+      <ul className="flex items-center gap-16">
         <li>
-          <Link href="/cabins" className="hover:text-accent-400 transition-colors">
+          <Link
+            href="/cabins"
+            className="transition-colors hover:text-accent-400"
+          >
             Cabins
           </Link>
         </li>
         <li>
-          <Link href="/about" className="hover:text-accent-400 transition-colors">
+          <Link
+            href="/about"
+            className="transition-colors hover:text-accent-400"
+          >
             About
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="flex items-center gap-4 transition-colors hover:text-accent-400"
+            >
+              <img
+                className="h-8 rounded-full"
+                src={session.user.image}
+                alt={session.user.name}
+                referrerPolicy="no-referrer"
+              />
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="transition-colors hover:text-accent-400"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
