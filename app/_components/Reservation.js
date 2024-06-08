62c49044e79024dedd68387a2915c606bf0ec7f3
thiagopdo <thiago.falconer@gmail.com>
2024-06-08 02:27:40 -0300

    Feat: added message to login when loggedout conditioning calendar and form

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,22 +1,30 @@
+import { auth } from "../_lib/auth";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
-import {
-  getBookedDatesByCabinId,
-  getSettings,
-} from "@/app/_lib/data-service";
+import { getBookedDatesByCabinId, getSettings } from "@/app/_lib/data-service";
 
-async function Reservation({cabin}) {
+async function Reservation({ cabin }) {
   //avoiding blocking waterfall
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
 
+  const session = await auth();
 
   return (
     <div className="grid min-h-[400px] grid-cols-2 border border-primary-800">
-      <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
-      <ReservationForm cabin={cabin} />
+      <DateSelector
+        settings={settings}
+        bookedDates={bookedDates}
+        cabin={cabin}
+      />
+      {session.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}{" "}
     </div>
   );
 }
