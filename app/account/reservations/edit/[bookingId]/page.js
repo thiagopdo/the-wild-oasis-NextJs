ea9489846df081df7b44aea5bac34b9776efe6ee
thiagopdo <thiago.falconer@gmail.com>
2024-06-13 18:01:53 -0300

    feat: added edit route and edit component

---
+++ app/account/reservations/edit/[bookingId]/page.js
@@ -0,0 +1,54 @@
+import { getBooking, getCabin } from "@/app/_lib/data-service";
+
+export default async function Page({ params }) {
+  const { bookingId } = params;
+  const { numGuests, observations, cabinId } = await getBooking(bookingId);
+  const { maxCapacity } = await getCabin(cabinId);
+
+  return (
+    <div>
+      <h2 className="mb-7 text-2xl font-semibold text-accent-400">
+        Edit Reservation #{bookingId}
+      </h2>
+
+      <form className="flex flex-col gap-6 bg-primary-900 px-12 py-8 text-lg">
+        <div className="space-y-2">
+          <label htmlFor="numGuests">How many guests?</label>
+          <select
+            name="numGuests"
+            id="numGuests"
+            defaultValue={numGuests}
+            className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm"
+            required
+          >
+            <option value="" key="">
+              Select number of guests...
+            </option>
+            {Array.from({ length: maxCapacity }, (_, i) => i + 1).map((x) => (
+              <option value={x} key={x}>
+                {x} {x === 1 ? "guest" : "guests"}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="space-y-2">
+          <label htmlFor="observations">
+            Anything we should know about your stay?
+          </label>
+          <textarea
+            name="observations"
+            defaultValue={observations}
+            className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm"
+          />
+        </div>
+
+        <div className="flex items-center justify-end gap-6">
+          <button className="bg-accent-500 px-8 py-4 font-semibold text-primary-800 transition-all hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+            Update reservation
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
