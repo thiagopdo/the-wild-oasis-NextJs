4de1a4fa6becaea4935a9102efb115c0caec7105
thiagopdo <thiago.falconer@gmail.com>
2024-06-18 12:22:21 -0300

    feat: useOptimistic to delete reservation

--- app/_components/ReservationList.js
+++ app/_components/ReservationList.js
@@ -1,15 +1,34 @@
-"use client"
+"use client";
 
 import ReservationCard from "./ReservationCard";
+import { deleteReservation } from "../_lib/actions";
 
-function ReservationList({bookings}) {
+import { useOptimistic } from "react";
+
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (curBookings, bookingId) => {
+      return curBookings.filter((booking) => booking.id !== bookingId);
+    },
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId);
+    await deleteReservation(bookingId);
+  }
+
   return (
     <ul className="space-y-6">
-      {bookings.map((booking) => (
-        <ReservationCard booking={booking} key={booking.id} />
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          onDelete={handleDelete}
+          booking={booking}
+          key={booking.id}
+        />
       ))}
     </ul>
   );
 }
 
-export default ReservationList
+export default ReservationList;
